9ecf9991bee2c672343b3e6827dbead1c7218d48
Alanis Yates <yatesalanis@gmail.com>
2022-10-13 14:43:55 -0400

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
       <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-      <p className="text-gray text-lg">React and Tailwind CSS in action</p>
+      <p className="text-gray text-lg">React and Tailwind CSS in action yo</p>
     </div>
   );
 }
